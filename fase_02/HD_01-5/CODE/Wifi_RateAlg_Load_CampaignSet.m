06f63574bb99d5b36d8106a2bb5a11d448d480df
danielrluna <danielro@ufrn.edu.br>
2019-07-11 12:20:36 -0300

    adicionando fase02 hd1.5 part 2: criando campanhas e simulando

---
+++ fase_02/HD_01-5/CODE/Wifi_RateAlg_Load_CampaignSet.m
@@ -0,0 +1,10 @@
+%%%%  common parameters
+global shortGuardIntervalG@ "false"
+    global staManagerG@ "ns3::AarfWifiManager"
+    global staManagerG@ "ns3::MinstrelHtWifiManager"
+    global staManagerG@ "ns3::MinstrelWifiManager"
+global shortGuardIntervalG@ "true"
+    global staManagerG@ "ns3::AarfWifiManager"
+    global staManagerG@ "ns3::MinstrelHtWifiManager"
+    global staManagerG@ "ns3::MinstrelWifiManager"
+
